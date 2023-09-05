129bf6047b9d1add65400c0e1c744aa1145ce6b1
CokerAlcocer <20203tn066@utez.edu.mx>
2023-09-04 21:32:48 -0600

    [UPDATE] Adición del GET singular, PUT, DELETE y alertas de estado

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 
-const getPeople = () => {
-    fetch('https://reqres.in/api/users', {
+const getPeople = async () => {
+    await fetch('https://reqres.in/api/users', {
         method: 'GET'
     }).then(res => res.json()).then(res => {
         let tbody = document.getElementById('tbody');
         let content = '';
 
         res.data.forEach((item, index) => {
-            content += `<tr class="text-light">
+            content += `<tr class="">
                             <td>${index + 1}</td>
                             <td><img src="${item.avatar}" alt=""></td>
                             <td>${item.first_name}</td>
                             <td>${item.last_name}</td>
                             <td>${item.email}</td>
+                            <td class="text-center">
+                                <button class="btn btn-primary" onclick="getPerson(${item.id})" data-bs-toggle="modal" data-bs-target="#update">Editar</button>
+                                <button class="btn btn-danger" onclick="destroyPerson(${item.id})">Eliminar</button>
+                            </td>
                         </tr>`;
         });
 
@@ -23,7 +27,7 @@ const getPeople = () => {
 
 getPeople();
 
-const savePerson = () => {
+const savePerson = async () => {
     let person = {
         first_name: document.getElementById('c_name').value,
         last_name: document.getElementById('c_last').value,
@@ -31,32 +35,89 @@ const savePerson = () => {
         avatar: document.getElementById('c_url').value
     }
 
-    fetch('https://reqres.in/api/users', {
+    await fetch('https://reqres.in/api/users', {
         method: 'POST',
         headers: {
             "Content-Type": "appliaction/json"
         },
         body: JSON.stringify(person)
-    }).then(res => {
+    }).then(() => {
+        getRegisterResponse();
+    }).catch(() => {
+        getErrorResponse();
+    });
+}
 
+const getPerson = async id => {
+    await fetch('https://reqres.in/api/users/'+id, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "appliaction/json"
+        }
+    }).then(res => res.json()).then(res => {
+        document.getElementById('u_name').value = res.data.first_name;
+        document.getElementById('u_last').value = res.data.last_name;
+        document.getElementById('u_email').value = res.data.email;
+        document.getElementById('u_url').value = res.data.avatar;
     }).catch(console.log);
 }
 
-const updatePerson = id => {
+const updatePerson = async id => {
     let person = {
-        first_name: document.getElementById('c_name').value,
-        last_name: document.getElementById('c_last').value,
-        email: document.getElementById('c_email').value,
-        avatar: document.getElementById('c_url').value
+        first_name: document.getElementById('u_name').value,
+        last_name: document.getElementById('u_last').value,
+        email: document.getElementById('u_email').value,
+        avatar: document.getElementById('u_url').value
     }
 
-    fetch('https://reqres.in/api/users', {
-        method: 'POST',
+    await fetch('https://reqres.in/api/users/'+id, {
+        method: 'PUT',
         headers: {
             "Content-Type": "appliaction/json"
         },
         body: JSON.stringify(person)
-    }).then(res => {
-        
-    }).catch(console.log);
+    }).then(() => {
+        getUpdateResponse();
+    }).catch(() => {
+        getErrorResponse();
+    });
+}
+
+const destroyPerson = async id => {
+    await fetch('https://reqres.in/api/users/'+id, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "appliaction/json"
+        }
+    }).then(() => {
+        getDeleteResponse();
+    }).catch(() => {
+        getErrorResponse();
+    });
+}
+
+const getRegisterResponse = () => {
+    document.getElementById('a_create').classList.remove('hidden');
+    document.getElementById('a_update').classList.add('hidden');
+    document.getElementById('a_delete').classList.add('hidden');
+    document.getElementById('a_error').classList.add('hidden');
+}
+
+const getUpdateResponse = () => {
+    document.getElementById('a_create').classList.add('hidden');
+    document.getElementById('a_update').classList.remove('hidden');
+    document.getElementById('a_delete').classList.add('hidden');
+    document.getElementById('a_error').classList.add('hidden');
+}
+
+const getDeleteResponse = () => {
+    document.getElementById('a_create').classList.add('hidden');
+    document.getElementById('a_update').classList.add('hidden');
+    document.getElementById('a_delete').classList.remove('hidden');
+    document.getElementById('a_error').classList.add('hidden');
+}
+
+const getErrorResponse = () => {
+    document.getElementById('a_delete').classList().add('hidden');
+    document.getElementById('a_error').classList().remove('hidden');
 }
